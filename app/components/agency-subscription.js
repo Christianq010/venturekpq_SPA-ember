a3168c66f48d615edf35de796302560e15e6c330
Christiaan Quyn <christianq010@gmail.com>
2017-04-02 14:18:30 +0530

    feat: Add Response message to Subscription Button with valid email address refact: Cchange footer contact links to Contact Us

--- app/components/agency-subscription.js
+++ app/components/agency-subscription.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    isDisabled: true
+    emailAddress: '',
+
+    //Check validity of String used as an Email Address
+    isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+    isDisabled: Ember.computed.not('isValid'),
+
+    actions: {
+
+        saveEmail() {
+            alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+            this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+            this.set('emailAddress', '');
+        }
+    }
 });
