29582a92267219a615cfba136da2c5ca79ba1440
Christiaan Quyn <christianq010@gmail.com>
2017-04-03 11:43:18 +0530

    doc: Comments on deprecated Subscription Component

--- app/components/agency-subscription.js
+++ app/components/agency-subscription.js
@@ -1,26 +1,27 @@
 import Ember from 'ember';
 
+//Deprecated since Index Controller used for this functionality instead
 export default Ember.Component.extend({
-    responseMessage: '',
-    emailAddress: '',
-
-    //Check validity of String used as an Email Address
-    isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-    isDisabled: Ember.computed.not('isValid'),
-
-    actions: {
-
-        saveEmail() {
-            const email = this.get('emailAddress');
-
-            const newSubscription = this.store.createRecord('subscription-model', {
-                email: email
-            });
-
-            newSubscription.save().then((response) => {
-                this.set('responseMessage', `Thank you! We've just saved your email address: ${response.get('emailAddress')}`);
-                this.set('emailAddress', '');
-            });
-        }
-    }
+    // responseMessage: '',
+    // emailAddress: '',
+    //
+    // //Check validity of String used as an Email Address
+    // isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+    // isDisabled: Ember.computed.not('isValid'),
+    //
+    // actions: {
+    //
+    //     saveEmail() {
+    //         const email = this.get('emailAddress');
+    //
+    //         const newSubscription = this.store.createRecord('subscription-model', {
+    //             email: email
+    //         });
+    //
+    //         newSubscription.save().then((response) => {
+    //             this.set('responseMessage', `Thank you! We've just saved your email address: ${response.get('emailAddress')}`);
+    //             this.set('emailAddress', '');
+    //         });
+    //     }
+    // }
 });
