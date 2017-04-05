24f23c957d2e4328bfc8858643cc41faad10d65d
Christiaan Quyn <christianq010@gmail.com>
2017-04-05 10:28:06 +0530

    fix: Subscribe input for Email bottom margin, Form submitted alert message changed

--- app/components/agency-contacts.js
+++ app/components/agency-contacts.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     actions: {
         submit: function() {
-            alert('Form submit!');
+            alert('Thank you, your message is being processed');
         }
     }
 });
